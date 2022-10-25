c721b9f31000f628c676a13db69484aff52970df
Richard Walton <richardwalton89@gmail.com>
2022-10-25 15:48:55 +0200

    modified json error response from getData, so it just have one error message
    Moved authorization to new function
    
    added new route for authorization
    
    users will need to call authorization function to grant access to api

--- libraries/ga.js
+++ libraries/ga.js
@@ -1,79 +1,112 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import {google} from "googleapis";
+import { google } from "googleapis";
 
 const email = process.env.CLIENT_EMAIL;
 const key = process.env.PRIVATE_KEY.replace(new RegExp("\\\\n"), "\n");
 const scopes = ["https://www.googleapis.com/auth/analytics.readonly"];
+const viewId = process.env.VIEW_ID;
 
 //Get Google analytics v3
 const analytics = google.analytics("v3");
-const viewId = process.env.VIEW_ID;
-const jwt = new google.auth.JWT({
-  email,
-  key,
-  scopes,
-});
+let jwt;
 
-async function getData(data, startDate="30daysAgo", endDate="today"){
-  const response = await jwt.authorize();
+async function authorizeAccess() {
+  jwt = new google.auth.JWT({
+    email,
+    key,
+    scopes
+  });
 
+  const response = await jwt.authorize().catch((err) => {
+    console.log("Error authorizing: ", err);
+    const {response} = err
+    const error = {details:response.data, status:response.status, statusText:response.statusText}
+    return error;
+  });
+
+  return response;
+}
+
+async function getData(data, startDate = "30daysAgo", endDate = "today") {
+  // const response = await jwt.authorize();
+
+  // jwt.authorize((err, tokens) => {
+  //   if (err) {
+  //     console.log("ga.js getData Error authorizing: ", err);
+  //     return { error: "Authorization failed." };
+  //   }
+
+  // });
   // is startDate and endDate not numbers?
-  const start =  isNaN(Number(startDate)) ? startDate : `${startDate}daysAgo`;
+  const start = isNaN(Number(startDate)) ? startDate : `${startDate}daysAgo`;
   const end = isNaN(Number(endDate)) ? endDate : `${endDate}daysAgo`;
 
-
-  const prop = {
-    'auth':jwt,
-    'ids':`ga:${viewId}`,
-    'start-date':start,
-    'end-date':end,
-    ...data
+  if (!jwt) {
+    return {
+      error: "Unauthorized. Call authorizeAccess before requesting api.",
+    };
   }
 
-  try{
-    const result = await analytics.data.ga.get(prop)
+  try {
+    const result = await analytics.data.ga.get({
+      auth: jwt,
+      ids: `ga:${viewId}`,
+      "start-date": start,
+      "end-date": end,
+      ...data,
+    });
     return result;
-
-  }catch(err){
-    console.log("There was an error:", err);
-    return {errorMessage:err.errors}
+  } catch (err) {
+    const { response } = err;
+    console.log("There was an error \n", err);
+    return {
+      error: response.data.error.message,
+      status: response.status,
+      statusText: response.statusText,
+    };
   }
 }
 
-async function getMetric(metric, startDate, endDate){
-  const data = await getData({metrics:metric}, startDate, endDate);
+async function getMetric(metric, startDate, endDate) {
+  const data = await getData({ metrics: metric }, startDate, endDate);
 
-  if(data.errorMessage){
-    return data.errorMessage;
+  if (data.error) {
+    return data;
   }
-  
+
   const result = {
-    value:parseInt(data.data.totalsForAllResults[metric], 10),
-    startDate: data.data.query['start-date'],
-    endDate: data.data.query['end-date']
+    value: parseInt(data.data.totalsForAllResults[metric], 10),
+    startDate: data.data.query["start-date"],
+    endDate: data.data.query["end-date"],
   };
-  return result
+  return result;
 }
 
-async function getDimensions(metric, dimension, startDate, endDate){
-  const data = await getData({metrics: metric, dimensions: dimension}, startDate, endDate);
+async function getDimensions(metric, dimension, startDate, endDate) {
+  const data = await getData(
+    { metrics: metric, dimensions: dimension },
+    startDate,
+    endDate
+  );
+
+  if (data.error) {
+    return data;
+  }
+
   let rows = data.data.rows;
 
-  if(data.errorMessage){
-    return data.errorMessage;
-  }
-
-  rows = rows.map(elem =>{ return {dimension: elem[0], value: elem[1]}})
+  rows = rows.map((elem) => {
+    return { dimension: elem[0], value: elem[1] };
+  });
 
   const result = {
-    array:rows,
-    startDate: data.data.query['start-date'],
-    endDate: data.data.query['end-date']
-  }
+    array: rows,
+    startDate: data.data.query["start-date"],
+    endDate: data.data.query["end-date"],
+  };
 
   return result;
-
 }
 
-export {getMetric, getDimensions}
+export { authorizeAccess, getMetric, getDimensions };
--- server.js
+++ server.js
@@ -3,27 +3,43 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 
-import { getMetric, getDimensions } from "./libraries/ga.js";
+import { authorizeAccess, getMetric, getDimensions } from "./libraries/ga.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/accessToken", async (req, res) => {
+  console.log("accessToken called")
+
+  const response = await authorizeAccess();
+
+  console.log("response: ", response);
+
+  res.send({response})
+
+})
+
 app.get("/api/metrics", async (req, res) => {
   const { metric, startDate, endDate } = req.query;
+  console.log("Requested route /api/metrics");
   console.log(`Requested metrics: ${metric}`);
   console.log(`Requested start-data: ${startDate}`);
   console.log(`Requested end-date: ${endDate}`);
 
   const body = await getMetric(`ga:${metric}`, startDate, endDate);
 
-  console.log("body:", body);
+  if(body.error){
+    res.status(body.status || 400).json({body})
+    return;
+  }
 
   res.send({ data: body });
 });
 
 app.get("/api/dimensions", async (req, res) => {
   const {metric, dimension, startDate, endDate } = req.query;
+  console.log("Requested route /api/dimensions");
   console.log(`Requested metric: ${metric}`);
   console.log(`Requested dimensions: ${dimension}`);
   console.log(`Requested startDate: ${startDate}`);
@@ -31,10 +47,15 @@ app.get("/api/dimensions", async (req, res) => {
 
   const body = await getDimensions(`ga:${metric}`, `ga:${dimension}`, startDate, endDate);
 
+  if(body.error){
+    res.status(body.status || 400).json({body})
+    return;
+  }
+
   res.send({data: body})
 });
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 8080;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
